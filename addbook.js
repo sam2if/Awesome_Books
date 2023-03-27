4a6d3738673f9fcdcf99bc85390c67a9492c47ab
sami <samif3443@gmail.com>
2023-03-27 05:59:31 -0700

    Update addbook.js

--- addbook.js
+++ addbook.js
@@ -1,38 +1,39 @@
 const button = document.querySelector('#button');
 const store = document.querySelector('#myList');
+var title = document.getElementById('title');
+var author = document.getElementById('author');
 
 let Book = []
 
+function Booklist(title, author, id) {
+    this.title = title.value;
+    this.author = author.value;
+    this.id = Book.length;
+}
 
-
-function remo(arr, index) {
-    arr.splice(index, 1);
+function remove(b) {
+    var filt = Book.filter((a, i) => {
+        if (b == a.id) {
+            Book.splice(i, 1);
+            let rem = document.getElementById(`m${b}`);
+            rem.remove();
+        }
+    })
 }
 
 function show() {
-    var book = {
-        title: document.getElementById('title').value,
-        author: document.getElementById('author').value
-    };
-    
+    var id;
+    var book = new Booklist(title, author, id);
     Book.push(book);
     const node = document.createElement("li");
     let len = Book.length;
     for (let i = 0; i < len; i++) {
         node.setAttribute('id', `m${i}`);
-        node.innerHTML = `${Book[i].title} <br> ${Book[i].author} <br> <button id="remove-${i}">Remove</button><hr><br>`;
+        node.innerHTML = `${Book[i].title} <br> ${Book[i].author} <br> <button onclick=remove(${Book[i].id})>Remove</button><hr><br>`;
         store.appendChild(node);
     }
-    const rem = document.document.getElementById(`remove-${i}`);
-    for (let j = 0; j < rem.length; j++) {
-        rem.addEventListener('click', () => {
-            rem[j].parentElement.style.display = "none";
-            remo(Book, j);
-        })
-    }
     title.value = '';
-    author.value = '';
-    
+    author.value = ''; 
 }
 
 
