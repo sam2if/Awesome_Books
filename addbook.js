cdc0324c140885c71074f584d9e87a17a9583035
sami <samif3443@gmail.com>
2023-03-27 10:58:31 -0700

    store on local storage

--- addbook.js
+++ addbook.js
@@ -5,10 +5,8 @@ var author = document.getElementById('author');
 
 let Book = [];
 
-// Check if there is saved data in local storage
 if (localStorage.getItem('bookList')) {
   Book = JSON.parse(localStorage.getItem('bookList'));
-  // Update the book list with the saved data
   updateBookList();
 }
 
@@ -26,7 +24,6 @@ function remove(b) {
       rem.remove();
     }
   });
-  // Save the updated book list to local storage
   localStorage.setItem('bookList', JSON.stringify(Book));
 }
 
@@ -43,25 +40,20 @@ function show() {
   }
   title.value = '';
   author.value = ''; 
-  // Save the updated book list to local storage
   localStorage.setItem('bookList', JSON.stringify(Book));
 }
 
 button.addEventListener("click", show);
 
-// Add an event listener for the storage event
 window.addEventListener('storage', function(event) {
   if (event.key === 'bookList') {
-    // Update the book list with the new data
     Book = JSON.parse(event.newValue);
     updateBookList();
   }
 });
 
 function updateBookList() {
-  // Clear the existing book list
   store.innerHTML = '';
-  // Create a new book list from the saved data
   let len = Book.length;
   for (let i = 0; i < len; i++) {
     const node = document.createElement("li");
