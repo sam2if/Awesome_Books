2437b707c607145bfb71fea150697ea0e607368e
sami <samif3443@gmail.com>
2023-03-28 12:07:14 -0700

    Update class.js

--- class.js
+++ class.js
@@ -1,59 +1,64 @@
 const button = document.querySelector('#button');
 const store = document.querySelector('#myList');
-var title = document.getElementById('title');
-var author = document.getElementById('author');
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+
+class Booklist {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.id = Booklist.count++;
+  }
+
+  remove(b) {
+    var filt = Book.filter((a, i) => {
+      if (b == a.id) {
+        Book.splice(i, 1);
+        let rem = document.getElementById(`m${b}`);
+        rem.remove();
+        return true;
+      }
+      return false;
+    });
+    localStorage.setItem('bookList', JSON.stringify(Book));
+    return this;
+  }
+
+  show() {
+    let book = new Booklist(titleInput.value, authorInput.value);
+    Book.push(book);
+    const node = document.createElement("li");
+    node.setAttribute('id', `m${book.id}`);
+    node.innerHTML = `${book.title} <br> ${book.author} <br> <button onclick="book.remove(${book.id})">Remove</button><hr><br>`;
+    store.appendChild(node);
+    titleInput.value = '';
+    authorInput.value = '';
+    localStorage.setItem('bookList', JSON.stringify(Book));
+    return this;
+  }
+
+  static count = 0;
+
+  static updateBookList() {
+    store.innerHTML = '';
+    let len = Book.length;
+    for (let i = 0; i < len; i++) {
+      const node = document.createElement("li");
+      node.setAttribute('id', `m${Book[i].id}`);
+      node.innerHTML = `${Book[i].title} <br> ${Book[i].author} <br> <button onclick="book.remove(${Book[i].id})">Remove</button><hr><br>`;
+      store.appendChild(node);
+    }
+    return this;
+  }
+ 
+}
 
 let Book = [];
 
 if (localStorage.getItem('bookList')) {
   Book = JSON.parse(localStorage.getItem('bookList'));
-  updateBookList();
+  Booklist.updateBookList();
 }
 
-class Booklist {
-    constructor(title, author, id) {
-      this.title = title.value;
-      this.author = author.value;
-      this.id = Book.length;
-    }
-  }
-
-function remove(b) {
-  var filt = Book.filter((a, i) => {
-    if (b == a.id) {
-      Book.splice(i, 1);
-      let rem = document.getElementById(`m${b}`);
-      rem.remove();
-    }
-  });
-  localStorage.setItem('bookList', JSON.stringify(Book));
-}
-
-function show() {
-  var id;
-  var book = new Booklist(title, author, id);
-  Book.push(book);
-  const node = document.createElement("li");
-  let len = Book.length;
-  for (let i = 0; i < len; i++) {
-    node.setAttribute('id', `m${i}`);
-    node.innerHTML = `${Book[i].title} <br> ${Book[i].author} <br> <button onclick=remove(${Book[i].id})>Remove</button><hr><br>`;
-    store.appendChild(node);
-  }
-  title.value = '';
-  author.value = ''; 
-  localStorage.setItem('bookList', JSON.stringify(Book));
-}
-
-button.addEventListener("click", show);
-
-function updateBookList() {
-  store.innerHTML = '';
-  let len = Book.length;
-  for (let i = 0; i < len; i++) {
-    const node = document.createElement("li");
-    node.setAttribute('id', `m${i}`);
-    node.innerHTML = `${Book[i].title} <br> ${Book[i].author} <br> <button onclick=remove(${Book[i].id})>Remove</button><hr><br>`;
-    store.appendChild(node);
-  }
-}
+const book = new Booklist(title, author);
+button.addEventListener("click", () => book.show());
