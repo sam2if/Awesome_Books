1e9d6a2ba655eaca2ddd8e00eab03c8aeb771a1a
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-03-28 19:45:59 +0530

    Modify navigation.js

---
+++ navigation.js
@@ -0,0 +1,4 @@
+const button = document.querySelector("#button");
+const store = document.querySelector("#myList");
+var title = document.getElementById("title");
+var author = document.getElementById("author");
