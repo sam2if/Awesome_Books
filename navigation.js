512a86d8d77454d98f809ecddf7cda1ff3c8ff6c
sami <samif3443@gmail.com>
2023-03-29 04:13:26 -0700

    Updating navigation.js file

--- navigation.js
+++ navigation.js
@@ -1,4 +1,37 @@
-const button = document.querySelector("#button");
-const store = document.querySelector("#myList");
-var title = document.getElementById("title");
-var author = document.getElementById("author");
+const list = document.querySelector('.list');
+const addnew = document.querySelector('.addnew');
+const contact = document.querySelector('.contacts');
+const forms = document.querySelector('.forms');
+const lll = document.querySelector('.lll');
+
+list.addEventListener('click', () => {
+  forms.style.display = 'none';
+  store.style.display = 'block';
+  store.style.border = '2px solid black';
+  lll.textContent = ' All awesome books';
+  list.style.color = 'rgb(82, 82, 234)';
+  contact.style.color = 'black';
+  addnew.style.color = 'black';
+  updateBookList()
+})
+
+addnew.addEventListener('click', () => {
+  store.style.display = 'none';
+  forms.style.display = 'flex';
+  addnew.style.color = 'rgb(82, 82, 234)';
+  list.style.color = 'black';
+  contact.style.color = 'black';
+  lll.textContent = 'Add a new book';
+  updateBookList();
+})
+
+contact.addEventListener('click', () => {
+  store.style.display = 'block';
+  lll.textContent = 'Contact information';
+  forms.style.display = 'none';
+  contact.style.color = 'rgb(82, 82, 234)';
+  addnew.style.color = 'black';
+  list.style.color = 'black';
+  store.innerHTML = 'dcvwgvwejdvwejdvwjevdwefefewfbevfhjwbfjwhbejhwekcwhecwecblwevcvegcvegwvcewcwehcewjcowecw';
+  store.style.border = 'none';
+})
